706d6d2a1f3770cf2c1d8983e6dc6f8326b6cde0
Willys Varela <q_will_p@hotmail.com>
2019-09-05 17:04:48 -0400

    Criando Views

---
+++ src/Pages/MainPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Header, Text} from 'native-base'
+const MainPage = () => {
+    return (
+        <Container>
+            <Header>
+                <Text>PontuApp</Text>
+            </Header>
+        </Container>
+    )
+}
+export default MainPage

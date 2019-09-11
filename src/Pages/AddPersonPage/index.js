6529d789b6cc5ce06cbe145cf9c12b96387ccc59
Willys <willysvarela@gmail.com>
2019-09-11 16:20:37 -0400

    AddPersonPage Form

--- src/Pages/AddPersonPage/index.js
+++ src/Pages/AddPersonPage/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
-import {Container, Content, Header, Form} from 'native-base';
+import {
+  Container,
+  Content,
+  Header,
+  Left,
+  Body,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+} from 'native-base';
 import getRealm from '../config/realm';
 
 const AddPersonPage = props => {
+  const [name, setName] = useState('initialState');
+
+  const handleAddPerson = () => {};
+  const handleKeyPress = e => {};
   return (
     <Container>
-      <Header></Header>
-      <Content></Content>
+      <Header transparent>
+        <Left />
+        <Body>
+          <Text>Adicionar Pessoa</Text>
+        </Body>
+      </Header>
+      <Content />
       <Form>
-
+        <Item floatingLabel>
+          <Label>Nome</Label>
+          <Input
+            placeholder="Nome"
+            value={name}
+            onKeyPress={e => handleKeyPress(e)}
+          />
+        </Item>
+        <Button onPress={() => handleAddPerson()} block>
+          Salvar
+        </Button>
       </Form>
     </Container>
   );
 };
+
+export default AddPersonPage;

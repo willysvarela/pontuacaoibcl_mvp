243ae98ebba7bfeab0e4fb4ca2b272c3d47c8ec2
Willys Varela <q_will_p@hotmail.com>
2019-09-11 23:22:41 -0400

    preparing merge

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
-import {createStackNavigator, createAppContainer} from 'react-navigation'
-import MainPage from './src/Pages/MainPage'
-import PersonPage from './src/Pages/PersonPage'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import MainPage from './src/Pages/MainPage/'
+import PersonPage from './src/Pages/PersonPage/'
 const MainNavigator = createStackNavigator({
-  Home: {}
+  Home: { screen: PersonPage },
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;

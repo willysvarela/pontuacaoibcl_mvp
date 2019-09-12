2fb35e031fbbfb4905b26dcdeff8e0115e1db699
Willys <willysvarela@gmail.com>
2019-09-12 12:47:42 -0400

    Add Person done

--- src/Pages/AddPersonPage/index.js
+++ src/Pages/AddPersonPage/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import {
   Container,
   Content,
@@ -13,40 +13,61 @@ import {
   Label,
   Button,
   Icon,
+  Toast,
 } from 'native-base';
-import getRealm from '../../config/realm';
+import personCtrl from '../../controllers/personController';
 
 const AddPersonPage = props => {
-  const [name, setName] = useState('initialState');
+  const [name, setName] = useState('');
+  const handleChangeText = e => {
+    setName(e);
+    console.log(e);
+  };
+  const handleAddPerson = async () => {
+    const persons = await personCtrl.readPersons();
+    const person = {
+      id: persons.length + 5,
+      name: name,
+    };
+    console.log(person);
+    const data = await personCtrl.savePerson(person);
+    console.log(data);
+    Toast.show({
+      text: 'Pessoa Salva',
+      buttonText: 'OK',
+      duration: 2000,
+    });
+  };
 
-  const handleAddPerson = () => { };
-  const handleKeyPress = e => { };
   return (
     <Container>
-      <Header transparent>
+      <Header>
         <Left>
           <Button onPress={() => props.navigation.goBack()}>
-            <Icon name="back" />
+            <Text>Voltar</Text>
           </Button>
         </Left>
         <Body>
           <Text>Adicionar Pessoa</Text>
         </Body>
       </Header>
-      <Content />
-      <Form>
-        <Item floatingLabel>
-          <Label>Nome</Label>
-          <Input
-            placeholder="Nome"
-            value={name}
-            onKeyPress={e => handleKeyPress(e)}
-          />
-        </Item>
+      <Content style={{flex: 1}}>
+        <View style={{flex: 1}}>
+          <Form style={{flex: 1, marginBottom: 10}}>
+            <Item floatingLabel>
+              <Label>Nome</Label>
+              <Input
+                placeholder=""
+                value={name}
+                onChangeText={handleChangeText}
+              />
+            </Item>
+          </Form>
+        </View>
         <Button onPress={() => handleAddPerson()} block>
-          Salvar
+          <Text>Salvar</Text>
         </Button>
-      </Form>
+      </Content>
     </Container>
   );
 };
--- src/Pages/PersonPage/index.js
+++ src/Pages/PersonPage/index.js
@@ -7,7 +7,7 @@
  * @flow
  */
 
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import commonColor from 'native-base/src/theme/variables/commonColor';
 import {
   Container,
@@ -26,21 +26,22 @@ import {
   Fab,
   Icon,
 } from 'native-base';
-import { View } from 'react-native';
+import {View} from 'react-native';
 //import getRealm from '../../config/realm';
 import personController from '../../controllers/personController';
 
 const PersonPage = props => {
   const [persons, setPersons] = useState([]);
+  const [refreshList, setRefreshList] = useState(false);
 
   useEffect(() => {
-    async function loadPersons() {
-      const res = await personController.readPersons();
-      setPersons(res);
-    }
     loadPersons();
   }, []);
 
+  async function loadPersons() {
+    const res = await personController.readPersons();
+    setPersons(res);
+  }
   /*  const getRealm = () => {
     return Realm.open({
       schema: [PersonSchema],
@@ -51,6 +52,12 @@ const PersonPage = props => {
     props.navigation.navigate('AddPerson');
     //await personController.savePerson({id: 4, name: 'Willys'});
   }
+
+  const handleRefresh = async e => {
+    setRefreshList(true);
+    await loadPersons();
+    setRefreshList(false);
+  };
 
   const renderPersonItem = person => {
     return (
@@ -93,12 +100,14 @@ const PersonPage = props => {
             <List
               dataArray={persons ? persons : []}
               renderRow={renderPersonItem}
-              keyExtractor={(person, id) => String(id)}
+              keyExtractor={person => String(person.id)}
+              refreshing={refreshList}
+              onRefresh={handleRefresh}
             />
           </Container>
         </View>
       </Content>
-      <Fab onPress={() => handlePressAddPerson()} style={{ marginBottom: 50 }}>
+      <Fab onPress={() => handlePressAddPerson()} style={{marginBottom: 50}}>
         <Icon name="add" />
       </Fab>
       <Footer>

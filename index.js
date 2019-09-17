943367e9ba03990c5d060f91f6082752f60312e4
Willys <willysvarela@gmail.com>
2019-09-17 12:50:39 -0400

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import {AppRegistry, View, Text} from 'react-native';
 import React from 'react';
 import {name as appName} from './app.json';
-import Routes from './src/Routes';
+
 const App = () => {
   return (
     <View>

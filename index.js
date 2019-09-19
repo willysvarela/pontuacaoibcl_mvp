af9cfea7c70fae8ff498f79fe30419513dc9a732
Willys <willysvarela@gmail.com>
2019-09-19 15:31:26 -0400

    readme update

--- index.js
+++ index.js
@@ -5,15 +5,4 @@ import React from 'react';
 import {AppRegistry, View, Text} from 'react-native';
 import Routes from './src/Routes';
 
-const App = () => {
-  return (
-    <View>
-      <Text>Olá</Text>
-      <Text>Olá</Text>
-      <Text>Olá</Text>
-      <Text>Olá</Text>
-    </View>
-  );
-};
-
 AppRegistry.registerComponent('pontuacaoibcl_mvp', () => Routes);
